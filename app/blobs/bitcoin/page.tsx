9d82652898fa243563018e63ad417ef6c9365c0a
laura <zhiyazhao@126.com>
2024-08-12 11:35:55 +0800

    1.0.16
    Fix the bug of loading more for eth/btc

--- app/blobs/bitcoin/page.tsx
+++ app/blobs/bitcoin/page.tsx
@@ -1,41 +1,39 @@
 "use client"
 import axios from "axios";
-import { useEffect, useState, useContext, useRef } from "react";
+import { useState, useContext, useRef, useCallback } from "react";
 import { Spin } from "antd";
 import { useRouter } from "next/navigation";
 import { If, Then, Else } from "react-if";
 import BlobSortCard from "@/app/components/blobsortcard/BlobSortCard";
 import BlobTable from "@/app/components/blobtable/BlobTable";
 import BlobContext from "../context";
-import { BotObserve } from "@/app/utils";
 import styles from "./page.module.css";
 import { IPagination, IBlob } from '@/app/types';
+import { useMount, useUpdateEffect } from "ahooks";
+import { debounce } from "lodash";
 
 export default function Page() { 
     const router = useRouter();
     const store = useContext(BlobContext);
-    const observeRef = useRef<BotObserve>();
-    // 卡片loading
-    const [cardLoading, setCardLoading] = useState<boolean>(true);
-    // 表格loading
-    const [tableLoading, setTableLoading] = useState<boolean>(true);
-    // 卡片分页
-    const [cardPagi, setCardPagi] = useState<IPagination>({
+
+    const [loading, setLoading] = useState(false);
+    // 分页
+    const paginationRef = useRef<IPagination>({
         page: 1,
-        per_page: 12,
-    });
-    // 表格分页
-    const [pagination, setPagination] = useState<IPagination>({
-        page: 1,
-        per_page: 12,
-       
-    });
+        per_page: 12
+    })
     // 表格总数据条数
     const [total, setTotal] = useState<number>(0)
     // 卡片列表数据
     const [blobList, setBlobList] = useState<Array<IBlob>>([]);
-    // 表格列表数据
-    const [tableList, setTableList] = useState<Array<IBlob>>([]);
+
+    function isScrollToBottom() {
+        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        var scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+        var clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
+        
+        return scrollTop + clientHeight >= scrollHeight;
+    }
 
     const apiGetBtcBlobList = async (params?: any) => {
         const res = await axios.get("/api_btc/show-blob", { params });
@@ -44,35 +42,9 @@ export default function Page() {
         }
     }
 
-    const dealDataForCard = async (params?: any) => {
-        setCardLoading(true);
-        const res = await apiGetBtcBlobList(params)
-        const { data } = res;
-        data?.sort((a: IBlob, b: IBlob) => {
-            const keyMap = new Map([
-                ['index', 'index'],
-                ['fee', 'fee'],
-                ['size', 'length'],
-                ['state', 'state']
-            ])
-            const key = keyMap.get(store.sortKey)
-            // state状态 invalid排在前面
-            if(store.sortKey === 'state'){
-                if(a['State'] !== 'valid' && b['State'] === 'valid'){
-                    return 1;
-                }else{
-                    return - 1;
-                }
-            }else{
-                if(key) return (b[key as keyof IBlob] as number) - (a[key as keyof IBlob] as number)
-            }
-        })
-        setCardLoading(false);
-        setBlobList([...blobList, ...data]);
-    }
-
-    const dealDataForTable = async (params: any) => {
-        setTableLoading(true);
+    const getAndTransferData = useCallback(debounce(async(params?: any) => {
+        if(loading) return;
+        setLoading(true)
         const res = await apiGetBtcBlobList(params)
         const { data, pagination } = res;
         data?.sort((a: IBlob, b: IBlob) => {
@@ -94,49 +66,79 @@ export default function Page() {
                 if(key) return (b[key as keyof IBlob] as number) - (a[key as keyof IBlob] as number)
             }
         })
-        setTotal(pagination.total ?? 0);
-        setTableLoading(false);
-        setTableList(data);
-    }
+        setTotal(pagination.total);
+        setLoading(false);
+        if(!data) {
+            return;
+        }
+        if(store.mode === 'list'){
+            setBlobList([...blobList, ...data]);
+        }else{
+            setBlobList(data);
+        }
+    }, 100), [blobList, total, store.mode])
 
     function handleRouter(commitment_hash: string, blob_id: number) {
         router.push(`/blobs/detail?commitment_hash=${commitment_hash}&chain=btc&id=${blob_id}`)
-        
     }
 
     function handlePaginate(current: number, _pageSize: number) {
-        setPagination({
-            ...pagination,
+        paginationRef.current = {
+            ...paginationRef.current,
             page: current
-        })
-    }
-
-    const handleLoadMore = () => {
-        const footer = document.getElementById('footer');
-        const getIn = () => {
-            setCardPagi({...cardPagi, page: cardPagi.page + 1})
         }
-        const observe = new BotObserve({node: footer, getIn})
-        return observe;
+        getAndTransferData(paginationRef.current);
     }
 
-    useEffect(() => {
-        if(!cardLoading && store.mode === 'list') {
-            observeRef.current = undefined;
-            observeRef.current = handleLoadMore();
-            return () => {
-                observeRef.current?.disconnect();
+    const handleLoadMore = useCallback(() => {
+        if(loading) return;
+        if(total <= paginationRef.current.per_page * paginationRef.current.page) return;
+        if(store.mode !== 'list') return;
+        const reachBottom = isScrollToBottom();
+        if(reachBottom){
+            paginationRef.current = {
+                ...paginationRef.current,
+                page: paginationRef.current.page + 1
+            }
+            getAndTransferData(paginationRef.current);
+        }
+    }, [loading, store.mode, total])
+
+    useMount(() => {
+        getAndTransferData(paginationRef.current);
+    })
+
+    useUpdateEffect(() => {
+        setBlobList([]);
+        setTotal(0);
+        paginationRef.current = {
+            page: 1,
+            per_page: 12
+        };
+    }, [store.mode])
+
+    useUpdateEffect(() => {
+        if(blobList?.length === 0 && total === 0){
+            getAndTransferData(paginationRef.current);
+        }
+    }, [total, blobList])
+
+    useUpdateEffect(() => {
+        if(loading){
+            document.removeEventListener('scroll', handleLoadMore)
+        }else{
+            if(total > blobList?.length && store.mode === 'list'){
+                setTimeout(() => {
+                    document.addEventListener('scroll', handleLoadMore)
+                }, 16);
+            }else{
+                document.removeEventListener('scroll', handleLoadMore)
             }
         }
-    }, [cardLoading])
-
-    useEffect(() => {
-        if(store.mode === 'list'){
-            dealDataForCard(cardPagi);
-        }else{
-            dealDataForTable(pagination);
+        return () => {
+            document.removeEventListener('scroll', handleLoadMore)
         }
-    }, [cardPagi, pagination])
+    }, [loading])
 
     return <div className={`flex flex-wrap ${styles.page_wrapper}`}>
         <div className={`fmt-16 mt-4 ${styles.list_wrapper}`}>
@@ -146,24 +148,26 @@ export default function Page() {
                         <Then>
                             {
                                 blobList.map((blob, index) => {
-                                   return <BlobSortCard  sortKey={store.sortKey} onClickHandle={() => {if(blob) handleRouter(blob.commitment_hash,blob.blob_id)}} key={index} blob={blob} chain='btc'/>
+                                   return <BlobSortCard sortKey={store.sortKey} onClickHandle={() => {if(blob) handleRouter(blob.commitment_hash,blob.blob_id)}} key={index} blob={blob} chain='btc'/>
                                 })
                             }
                         </Then>
                         <Else>
                             <div style={{width: '100%', marginTop: '100px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                                {cardLoading ? <Spin></Spin> : null}
+                                {loading ? <Spin></Spin> : null}
                             </div>
                         </Else>
                     </If>
                 </Then>
                 <Else>
-                    <BlobTable loading={tableLoading} blobList={tableList} pagination={{...pagination, total}} handlePaginate={handlePaginate} chain='btc'/>
+                    {
+                        <BlobTable loading={loading} blobList={blobList} pagination={{...paginationRef.current, total}} handlePaginate={handlePaginate} chain='btc'/>
+                    }
                 </Else>
             </If>
         </div>
         <div style={{width: '100%', marginTop: '16px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            {store.mode === 'list' && blobList?.length > 0 && cardLoading ? <Spin></Spin> : null}
+            {store.mode === 'list' && blobList?.length > 0 && loading ? <Spin></Spin> : null}
         </div>
     </div>;
 }
--- app/blobs/ethereum/page.tsx
+++ app/blobs/ethereum/page.tsx
@@ -1,89 +1,175 @@
 "use client"
 import axios from "axios";
-import { useEffect, useState, useContext, useCallback } from "react";
+import { useState, useContext, useRef, useCallback } from "react";
+import { Spin } from "antd";
 import { useRouter } from "next/navigation";
+import { If, Then, Else } from "react-if";
 import BlobSortCard from "@/app/components/blobsortcard/BlobSortCard";
-import BlobContext from "../context";
 import BlobTable from "@/app/components/blobtable/BlobTable";
-import styles from './page.module.css';
+import BlobContext from "../context";
+import styles from "./page.module.css";
 import { IPagination, IBlob } from '@/app/types';
-import { filter } from "lodash";
+import { useMount, useUpdateEffect } from "ahooks";
+import { debounce } from "lodash";
 
-export default function Page() {
+export default function Page() { 
     const router = useRouter();
     const store = useContext(BlobContext);
-    const [paginateParams, setPaginateParams] = useState<any>({
-        page: 1, 
-        per_page: 12,
-    })
-    const [pagination, setPagination] = useState<IPagination>({
-      
 
+    const [loading, setLoading] = useState(false);
+    // 分页
+    const paginationRef = useRef<IPagination>({
         page: 1,
-        per_page: 12,
-        total: 0
-    });
+        per_page: 12
+    })
+    // 表格总数据条数
+    const [total, setTotal] = useState<number>(0)
+    // 卡片列表数据
     const [blobList, setBlobList] = useState<Array<IBlob>>([]);
 
-
-    const apiGetEthBlobList = useCallback(async (params?: any) => {
-        const res = await axios.get("/api/show-blob", { params });
-        // const res = await axios.get("/api/eth-blobs", { params: {...params, filter: store.sortKey} });
-        if(res.status === 200){
-            let list = res.data.data;
-            list.sort((a: IBlob, b: IBlob) => {
-                const keyMap = new Map([
-                    ['index', 'index'],
-                    ['fee', 'fee'],
-                    ['size', 'length'],
-                    ['state', 'state']
-                ])
-                const key = keyMap.get(store.sortKey)
-                if(key) return (b[key as keyof IBlob] as number) - (a[key as keyof IBlob] as number)
-                
-            })
-            // for (let item in list) {
+    function isScrollToBottom() {
+        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        var scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+        var clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
         
-            // item.hash_show = getHash(item.commitment_hash) + ""
-            // item.age_show = getTimeSince(targetDate)
-            // item.v_show = Array.from(getHash(item.validators[0]) + "")
-            // item.store_show = isTimestampMoreThan14DaysAgo(targetDate) ? 'Invalid' : "Valid"
-            // }
-            console.error(list)
-            setBlobList(list);
-            setPagination(res.data.pagination)
+        return scrollTop + clientHeight >= scrollHeight;
+    }
+
+    const apiGetEthBlobList = async (params?: any) => {
+        const res = await axios.get("/api/show-blob", { params });
+        if(res.status === 200){
+            return res.data;
         }
-    }, [store.sortKey])
+    }
 
-    function handleRouter(commitment_hash: string, blob_id: number ) {
+    const getAndTransferData = useCallback(debounce(async(params?: any) => {
+        if(loading) return;
+        setLoading(true)
+        const res = await apiGetEthBlobList(params)
+        const { data, pagination } = res;
+        data.sort((a: IBlob, b: IBlob) => {
+            const keyMap = new Map([
+                ['index', 'index'],
+                ['fee', 'fee'],
+                ['size', 'length'],
+                ['state', 'state']
+            ])
+            const key = keyMap.get(store.sortKey)
+             // state状态 invalid排在前面
+             if(store.sortKey === 'state'){
+                if(a['State'] !== 'valid' && b['State'] === 'valid'){
+                    return 1;
+                }else{
+                    return - 1;
+                }
+            }else{
+                if(key) return (b[key as keyof IBlob] as number) - (a[key as keyof IBlob] as number)
+            }
+            
+        })
+        setTotal(pagination.total);
+        setLoading(false);
+        if(!data) {
+            return;
+        }
+        if(store.mode === 'list'){
+            setBlobList([...blobList, ...data]);
+        }else{
+            setBlobList(data);
+        }
+    }, 100), [blobList, total, store.mode])
+
+    function handleRouter(commitment_hash: string, blob_id: number) {
         router.push(`/blobs/detail?commitment_hash=${commitment_hash}&chain=eth&id=${blob_id}`)
-
     }
 
     function handlePaginate(current: number, _pageSize: number) {
-        setPaginateParams({
-            page: current,
-            per_page: 12,
-        })
+        paginationRef.current = {
+            ...paginationRef.current,
+            page: current
+        }
+        getAndTransferData(paginationRef.current);
     }
 
-    useEffect(() => {
-        apiGetEthBlobList({
-            ...paginateParams
-        })
-    }, [paginateParams, apiGetEthBlobList])
+    const handleLoadMore = useCallback(() => {
+        if(loading) return;
+        if(total <= paginationRef.current.per_page * paginationRef.current.page) return;
+        if(store.mode !== 'list') return;
+        const reachBottom = isScrollToBottom();
+        if(reachBottom){
+            paginationRef.current = {
+                ...paginationRef.current,
+                page: paginationRef.current.page + 1
+            }
+            getAndTransferData(paginationRef.current);
+        }
+    }, [loading, store.mode, total])
+
+    useMount(() => {
+        getAndTransferData(paginationRef.current);
+    })
+
+    useUpdateEffect(() => {
+        setBlobList([]);
+        setTotal(0);
+        paginationRef.current = {
+            page: 1,
+            per_page: 12
+        };
+    }, [store.mode])
+
+    useUpdateEffect(() => {
+        if(blobList?.length === 0 && total === 0){
+            getAndTransferData(paginationRef.current);
+        }
+    }, [total, blobList])
+
+    useUpdateEffect(() => {
+        if(loading){
+            document.removeEventListener('scroll', handleLoadMore)
+        }else{
+            if(total > blobList?.length && store.mode === 'list'){
+                setTimeout(() => {
+                    document.addEventListener('scroll', handleLoadMore)
+                }, 16);
+            }else{
+                document.removeEventListener('scroll', handleLoadMore)
+            }
+        }
+        return () => {
+            document.removeEventListener('scroll', handleLoadMore)
+        }
+    }, [loading])
 
     return <div className={`flex flex-wrap ${styles.page_wrapper}`}>
         <div className={`fmt-16 mt-4 ${styles.list_wrapper}`}>
-            {
-                store.mode === 'list' ? <>
-                {
-                    blobList.map((blob, index) => {
-                        return <BlobSortCard sortKey={store.sortKey} onClickHandle={() => {if(blob) handleRouter(blob.commitment_hash,blob.blob_id)}} key={index} blob={blob} chain='eth'/>
-                    })
-                }
-                </> : <BlobTable loading={false}  blobList={blobList} pagination={pagination} handlePaginate={handlePaginate} chain='eth'/>
-            }
+            <If condition={store.mode === 'list'}>
+                <Then>
+                    <If condition={blobList?.length > 0}>
+                        <Then>
+                            {
+                                blobList.map((blob, index) => {
+                                   return <BlobSortCard sortKey={store.sortKey} onClickHandle={() => {if(blob) handleRouter(blob.commitment_hash,blob.blob_id)}} key={index} blob={blob} chain='eth'/>
+                                })
+                            }
+                        </Then>
+                        <Else>
+                            <div style={{width: '100%', marginTop: '100px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                                {loading ? <Spin></Spin> : null}
+                            </div>
+                        </Else>
+                    </If>
+                </Then>
+                <Else>
+                    {
+                        <BlobTable loading={loading} blobList={blobList} pagination={{...paginationRef.current, total}} handlePaginate={handlePaginate} chain='eth'/>
+                    }
+                </Else>
+            </If>
+        </div>
+        <div style={{width: '100%', marginTop: '16px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            {store.mode === 'list' && blobList?.length > 0 && loading ? <Spin></Spin> : null}
         </div>
     </div>;
 }
+ 
